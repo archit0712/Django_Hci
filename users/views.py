447cb9ebab37b578fbc187a0f4384094ef6fa921
archit0712 <57456294+archit0712@users.noreply.github.com>
2023-02-11 00:42:58 -0500

    fix register

--- users/views.py
+++ users/views.py
@@ -2,16 +2,24 @@ from django.shortcuts import render , redirect
 from django.contrib import messages
 from .form import UserRegisterForm, UserUpdateForm ,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username} ! you can login now')
-            return redirect('login')
+            try :
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'Account Created for {username} ! you can login now')
+                return redirect('login')
+            except ObjectDoesNotExist :
+                    form.save()
+                    username = form.cleaned_data.get('username')
+                    messages.success(request, f'Account Created for {username} ! you can login now')
+                    return redirect('login')
+
     else :
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form' : form})
